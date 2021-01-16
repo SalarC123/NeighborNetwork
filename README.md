8b332397532e10b39bb42a46666d08f6b3e7c465
jessehu312 <jessehu312@gmail.com>
2021-01-16 15:25:24 -0800

    (#4) Connect Register to backend

--- README.md
+++ README.md
@@ -4,10 +4,21 @@
 # Local Development
 
 ## Initial
+
+[Setting up your virutal environment](https://flask.palletsprojects.com/en/1.1.x/installation/#create-an-environment)
+
+### Windows
 ```
 python -m pip install --user virtualenv
 virtualenv venv
 venv\Scripts\activate
+pip install -r requirements.txt
+```
+
+###  Linux / Mac
+```
+python3 -m venv venv
+. venv/bin/activate
 pip install -r requirements.txt
 ```
 
--- app/routes/api.py
+++ app/routes/api.py
@@ -5,15 +5,18 @@ from app.database import db
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 
-@blueprint.route('/register')
-@blueprint.route('/user')
-def user():
-    try:
-        id_token = request.headers.get('Authorization', '').split('Bearer ')[1]
-        decoded_token = auth.verify_id_token(id_token)
-    except:
-        abort(401)
+@blueprint.route('/register', methods=['POST'])
+def register():
+    payload = request.json
 
-    uid = decoded_token['uid']
-    email = decoded_token['email']
-    return {'debug': decoded_token}, 200
+    user = auth.create_user(**{
+        'email': payload['email'],
+        'password': payload['password']
+    })
+    zip_code = payload['zip_code']
+    return {'uid': user.uid, 'email': payload['email'], 'zip_code': zip_code}, 200
+
+
+@blueprint.route('/user', methods=['POST'])
+def user():
+    pass
