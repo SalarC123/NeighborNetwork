b03e50a770633f0795310dd1a6ffd8bab9b3081c
jessehu312 <jessehu312@gmail.com>
2021-01-16 23:46:25 -0800

    Google Login Stub

--- app/static/js/signup.js
+++ app/static/js/signup.js
@@ -1,4 +1,5 @@
 const signup = document.getElementById("sign-up");
+const signupGoogle = document.getElementById("sign-up-google");
 
 signup.onclick = (e) => {
   const emailElement = document.getElementById("email");
@@ -42,3 +43,34 @@ signup.onclick = (e) => {
 
   return false;
 };
+
+signupGoogle.onclick = (e) => {
+  firebase.auth().signInWithRedirect(googleAuthProvider);
+  return false;
+};
+
+firebase.auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      //var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      //var token = credential.accessToken;
+      // ...
+
+      // This does nothing because I don't think it's worth it to have two register pages
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
