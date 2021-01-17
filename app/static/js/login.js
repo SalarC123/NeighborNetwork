0b1430da2156e26fdf7168c34f880db87fb069d5
jessehu312 <64887515+jessehu312@users.noreply.github.com>
2021-01-17 09:47:54 -0800

    Google Login Stub (#24)
    * (#16) Heroku Deploy
    
    * Google Login Stub
    
    * Revert "(#16) Heroku Deploy"
    
    This reverts commit b2e70b0a37781a54f320dd0dda2fbcb64bb4d678.

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,4 +1,5 @@
 const login = document.getElementById("login");
+const loginGoogle = document.getElementById("login-google");
 
 login.onclick = (e) => {
   const emailElement = document.getElementById("email");
@@ -35,3 +36,33 @@ login.onclick = (e) => {
 
   return false;
 };
+
+loginGoogle.onclick = (e) => {
+  firebase.auth().signInWithRedirect(googleAuthProvider);
+  return false;
+};
+
+firebase.auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // ...
+
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
